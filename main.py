fa7a80f201b7d1bf877ed398df53ede7e9700003
swolfyguy <58931830+swolfyguy@users.noreply.github.com>
2021-06-23 22:39:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
+from typing import Optional
+import requests
+from fastapi import FastAPI
 
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+def get_value_byaction(action: str, value: int = 1):
+    if action == "buy" or action == "long":
+        return abs(value)
+    elif action == "sell" or action == "short":
+        return int(-abs(value))
+
+
+@app.post("/trade_tron")
+def read_item(stock_name: str, action: str, value: int,
+              baseurl: str = "https://api.tradetron.tech/api1.php",
+              auth_key: str = "173a11bf-f3be-47a8-8487-899f9269be50",
+              ):
+    data = {
+        "auth-token": auth_key,
+        "key": stock_name,
+        "value": get_value_byaction(action.lower(), value)
+    }
+
+    x = requests.request(method='POST', url=baseurl, data=data)
+    return {"response": x.text, "tradetron_request": {"url": baseurl, "data": data}}
